2b073c2b5493b34ce03cc9406173ae582295d47a
Vinay Kumar <vinaydagar514@gmail.com>
2025-03-30 14:33:53 +0530

    Implement authentication layout and sign-in/sign-up pages; update header component for improved navigation

--- components/header.jsx
+++ components/header.jsx
@@ -1,23 +1,79 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
 
-const Header = () => {
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false;
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src={"/logo.png"}
+            alt="Vehiql Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">admin</span>
+          )}
+        </Link>
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <>
+              <Link href="/">
+                <Button variant="outline" className="flex items-center gap-2">
+                  <ArrowLeft size={18} />
+                  <span>Back to App</span>
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button>
+                  <CarFront size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+              {!isAdmin ? (
+                <Link href="/reservations">
+                  <Button variant="outline">
+                    <Heart size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/reservations">
+                  <Button variant="outline">
+                    <Layout size={18} />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- middleware.js
+++ middleware.js
@@ -1,6 +1,23 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  auth,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/admin(.*)",
+  "/saved-cars(.*)",
+  "/reservations(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
