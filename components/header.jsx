720b29a520544964f5d5de36efe3b5a643095e17
Vinay Kumar <vinaydagar514@gmail.com>
2025-04-02 23:11:48 +0530

    Implement user role check in header component and add user management functionality

--- components/header.jsx
+++ components/header.jsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
 const Header = async ({ isAdminPage = false }) => {
-  const isAdmin = false;
+  const user = await checkUser();
+  const isAdmin = user.role === "ADMIN";
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="mx-auto px-4 py-4 flex items-center justify-between">
