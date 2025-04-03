1548921d414179f82df435e59bc21c833702def9
Vinay Kumar <vinaydagar514@gmail.com>
2025-04-03 22:56:26 +0530

    Add sidebar component to admin layout for navigation

--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -2,8 +2,9 @@ import { getAdmin } from "@/actions/admin";
 import Header from "@/components/header";
 import { notFound } from "next/navigation";
 import React from "react";
+import Sidebar from "./_components/sidebar";
 
-const AdminLayout = async () => {
+const AdminLayout = async ({ children }) => {
   const admin = await getAdmin();
 
   if (!admin.authorized) {
@@ -12,6 +13,10 @@ const AdminLayout = async () => {
   return (
     <div className="h-full">
       <Header isAdminPage={true} />
+      <div className="flex h-full w-56 flex-col top-20 fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      <main className="md:pl-56 pt-[80px] h-full">{children}</main>
     </div>
   );
 };
